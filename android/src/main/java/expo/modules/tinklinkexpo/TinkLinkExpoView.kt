54a80e09ba03ed9af24002cef236b9aacc222be6
eridr <erik@dreifaldt.com>
2023-05-14 00:41:28 +0200

    chore: remove TinkLinkExpoView.kt from android

--- android/src/main/java/expo/modules/tinklinkexpo/TinkLinkExpoView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.tinklinkexpo
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class TinkLinkExpoView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
